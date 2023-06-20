2d192fdd0c4fd076a56e92d4540bd6f1d5bbf956
Bhawani Shanker <133518238+bhawanishanker1999@users.noreply.github.com>
2023-06-20 12:51:51 +0530

    Paytm-Clone
    Using Html Css And Js

---
+++ app.js
@@ -0,0 +1,6 @@
+const headerBtnImg=document.getElementById("header-btn-img");
+
+const onHover = (event) => (headerBtnImg.src= "./assests/logoutImg.svg");
+const onLeave = (event) => (headerBtnImg.src= "./assests/asset 1.svg");
+
+
